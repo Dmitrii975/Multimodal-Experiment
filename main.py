2d7b044593774c6fb44536525643d2ba22cb7478
Дмитрий Колесник <ddimon27.03@gmail.com>
2025-11-09 21:07:25 +0300

    Дописал код для vae эксперимента, надо перекинуть на комп, обучить нормально

--- main.py
+++ main.py
@@ -13,23 +13,6 @@ from metrics import *
 def change_path(x):
     return 'data/flickr30k_images/flickr30k_images/' + x
 
-# if __name__ == 'main':
-    # df = pd.read_csv('data/flickr30k_images/results.csv', sep='|')
-    # df = df.dropna(axis=0)
-    # df['npth'] = df['image_name'].map(change_path)
-    # ndf = df[['npth', ' comment']]
-    # ndf[TYPE1_COLUMN_NAME] = TYPE_IMAGE
-    # ndf[TYPE2_COLUMN_NAME] = TYPE_TEXT
-    # ndf.rename(columns={'npth': OBJECT1_COLUMN_NAME, ' comment': OBJECT2_COLUMN_NAME}, inplace=True)
-
-    # print('New df formed')
-
-    # with open("data/id_image.pkl", "rb") as f:
-    #     id_image = pickle.load(f)
-
-    # with open("data/id_text.pkl", "rb") as f:
-    #     id_text = pickle.load(f)
-
 it = pd.read_csv('data/mnist/image.csv')
 at = pd.read_csv('data/mnist/audio.csv')
 
